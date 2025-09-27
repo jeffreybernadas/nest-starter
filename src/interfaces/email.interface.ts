e783f4371ca316f7757e66785723a1716c25b726
Jeffrey Bernadas <jeffrey.bernadas0@gmail.com>
2025-09-27 17:57:14 +0800

    feat(mailing): add resend config, modules, and service for emailing, and add test email template

---
+++ src/interfaces/email.interface.ts
@@ -0,0 +1,5 @@
+export interface TestEmailTemplateProps {
+  name?: string;
+  buttonText?: string;
+  buttonUrl?: string;
+}
---
+++ src/utils/email/email.util.ts
@@ -0,0 +1,32 @@
+import { render } from '@react-email/components';
+import { TestEmailTemplate } from '@/shared/mail/templates/test-email';
+import { TestEmailTemplateProps } from '@/interfaces/email.interface';
+
+export class EmailRenderer {
+  /**
+   * Renders the TestEmail template
+   * @param data - Optional data to pass to the template
+   * @returns Rendered HTML string
+   */
+  static async renderTestEmail(data?: TestEmailTemplateProps): Promise<string> {
+    return await render(TestEmailTemplate(data));
+  }
+
+  /**
+   * Generic method to render any email template
+   * @param templateName - Name of the template to render
+   * @param data - Data to pass to the template
+   * @returns Rendered HTML string
+   */
+  static async renderTemplate<T>(
+    templateName: string,
+    data?: T,
+  ): Promise<string> {
+    switch (templateName) {
+      case 'test-email':
+        return await this.renderTestEmail(data as TestEmailTemplateProps);
+      default:
+        throw new Error(`Unknown email template: ${templateName}`);
+    }
+  }
+}
